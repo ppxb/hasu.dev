03560a95a33f088c12c636b3bbfb698a95bc6815
ppxb <ppxb123@gmail.com>
2022-04-13 20:21:16 +0800

    update

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,10 +1,4 @@
-<script setup lang="ts">
-const name = ref('')
-const router = useRouter()
-const go = () => {
-  if (name.value) router.push(`/hi/${encodeURIComponent(name.value)}`)
-}
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div>
@@ -14,28 +8,7 @@ const go = () => {
       </a>
     </p>
     <p>
-      <em text-sm op75>Opinionated Vite Starter Template</em>
+      <em text-sm op75>I'm a full-stuck developer based in China.</em>
     </p>
-
-    <div py-4 />
-
-    <input
-      id="input"
-      v-model="name"
-      placeholder="What's your name?"
-      type="text"
-      autocomplete="false"
-      p="x-4 y-2"
-      w="250px"
-      text="center"
-      bg="transparent"
-      border="~ rounded gray-200 dark:gray-700"
-      outline="none active:none"
-      @keydown.enter="go"
-    />
-
-    <div>
-      <button class="m-3 text-sm btn" :disabled="!name" @click="go">Go</button>
-    </div>
   </div>
 </template>
