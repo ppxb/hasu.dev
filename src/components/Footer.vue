bf0814c42f86521dd039d2972f474e8dcac62667
ppxb <ppxb123@gmail.com>
2022-04-14 19:35:57 +0800

    update

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,14 +1,10 @@
 <template>
-  <div prose mt-10 mb-6 m-auto opacity-50 flex>
+  <div class="prose mt-10 mb-6 m-auto opacity-50 flex">
     <span text-sm>
       © 2022 - Build by
-      <a
-        target="_blank"
-        href="https://github.com/ppxb"
-        style="color: inherit"
-        hover:underline
-        >蓮</a
-      >
+      <a target="_blank" href="https://github.com/ppxb" style="color: inherit">
+        蓮
+      </a>
     </span>
     <div class="flex-auto" />
   </div>
